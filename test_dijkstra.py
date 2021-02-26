e3bcbd0310b2b931fd1947ebb2b8ce2119ad8e0e
Shin Yoon <shinmyung0@gmail.com>
2021-02-26 11:50:35 -0800

    slight changes

--- test_dijkstra.py
+++ test_dijkstra.py
@@ -1,13 +1,14 @@
 import math
+from collections import defaultdict
 
 
 def dijkstra(adj_matrix):
+    result = defaultdict(int)
+    return result
 
-    return None
 
-
-def shortest_path_between(source_node, dest_node):
-    return math.inf
+def shortest_path_to(path_table, dest_node):
+    return path_table[dest_node]
 
 
 def test_dijkstra():
@@ -15,8 +16,8 @@ def test_dijkstra():
     test_matrix = [[0, 1, 3, 0], [0, 0, 1, 5], [0, 0, 0, 1], [0, 0, 0, 0]]
     start_node = 0
     dest_node = 3
-    shortest_paths = dijkstra(0)
+    shortest_paths_table = dijkstra(0)
 
-    result = shortest_path_between(0, 3)
+    result = shortest_paths_table[3]
 
     assert result == 3
